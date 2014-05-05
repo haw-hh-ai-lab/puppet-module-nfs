97cedfdc2e8c836ff315f8fb8b824a4ddca056d2
Lutz Behnke <lutz.behnke@informatik.haw-hamburg.de>
2014-05-05 15:21:35 +0200

    add mountd option params

--- manifests/server.pp
+++ manifests/server.pp
@@ -40,12 +40,16 @@ class nfs::server (
   $nfs_v4_root_export_atboot    = false,
   $nfs_v4_root_export_options   = '_netdev',
   $nfs_v4_root_export_bindmount = undef,
-  $nfs_v4_root_export_tag       = undef
+  $nfs_v4_root_export_tag       = undef,
+  #
+  $mountd_port                   = undef,
+  $mountd_threads                = 1
 ) inherits nfs::params {
 
   class { "nfs::server::${::nfs::params::osfamily}":
     nfs_v4              => $nfs_v4,
     nfs_v4_idmap_domain => $nfs_v4_idmap_domain,
+    mount_port          => $mount_port,
   }
 
   include nfs::server::configure
