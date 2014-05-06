684329d6b8ac4bcc836a9da5a8c394870630f529
Lutz Behnke <lutz.behnke@informatik.haw-hamburg.de>
2014-05-06 15:26:38 +0200

    add dependency between shellvar['/etc/default/nfs-kernel-server'] and Service['nfs-kernel-server']; tweak test (WIP)

--- manifests/server/debian.pp
+++ manifests/server/debian.pp
@@ -19,7 +19,7 @@ class nfs::server::debian(
       target   => "/etc/default/nfs-kernel-server",
       variable => "RPCMOUNTDOPTS",
       value    => "--manage-gids --port ${mountd_port} --num-threads ${mountd_threads}",
-
+      notify   => Service['nfs-kernel-server'],
     }
   }
 
